c0bd289af0a11811173ddb602e4a1d8fd34bc63b
mecanass <mec.anass@gmail.com>
2023-12-01 23:24:02 -0500

    correction d'un bug dans la simulation lors du lancement de l'attaque

--- main.py
+++ main.py
@@ -9,6 +9,8 @@
 import os
 import sys
 import optparse
+import threading
+import multiprocessing
 import time
 from art import *
 from sumolib import checkBinary  # Checks for the binary in environ vars
@@ -31,80 +33,91 @@ def get_options():
     options, args = opt_parser.parse_args()
     return options
 
-# Attack qui met tour les feu ciblé au vert
+def show_lights_state():
+    F1 = traci.trafficlight.getRedYellowGreenState("F1")
+    F2 = traci.trafficlight.getRedYellowGreenState("F2")
+    F3 = traci.trafficlight.getRedYellowGreenState("F3")
+    F4 = traci.trafficlight.getRedYellowGreenState("F4")
+    print("Etat du feu rouge")
+    print("F1: " + F1)
+    print("F2: " + F2)
+    print("F3: " + F3)
+    print("F4: " + F4)
+
+
 def green_light_forever():
     traci.trafficlight.setRedYellowGreenState("F1",'GGGGG')
     traci.trafficlight.setRedYellowGreenState("F2",'GGGGG')
     traci.trafficlight.setRedYellowGreenState("F3",'GGGGG')
     traci.trafficlight.setRedYellowGreenState("F4",'GGGGGG')
-    reg_zero = client.read_holding_registers(0)
-    while reg_zero == 1:
-        normal_phase_scadabr()
+    normal_phase_scadabr()
+
+def step_up():
+        show_lights_state()
+        traci.simulationStep()
+        time.sleep(1)
 
 ## fonction pour montrer un fonctionnement normal dans scadaBR
 def normal_phase_scadabr():
-    client.write_single_coil(0,1)
-    client.write_single_coil(1,0)
-    client.write_single_coil(2,0)
-    client.write_single_coil(3,1)
-    client.write_single_coil(4,0)
-    client.write_single_coil(5,0)
-    client.write_single_coil(6,0)
-    client.write_single_coil(7,0)
-    client.write_single_coil(8,1)
-    client.write_single_coil(9,0)
-    client.write_single_coil(10,0)
-    client.write_single_coil(11,1)
-    time.sleep(15)
-    client.write_single_coil(0,0)
-    client.write_single_coil(1,1)
-    client.write_single_coil(2,0)
-    client.write_single_coil(3,0)
-    client.write_single_coil(4,1)
-    client.write_single_coil(5,0)
-    time.sleep(3)
-    client.write_single_coil(0,0)
-    client.write_single_coil(1,0)
-    client.write_single_coil(2,1)
-    client.write_single_coil(3,0)
-    client.write_single_coil(4,0)
-    client.write_single_coil(5,1)
-    client.write_single_coil(6,1)
-    client.write_single_coil(7,0)
-    client.write_single_coil(8,0)
-    client.write_single_coil(9,1)
-    client.write_single_coil(10,0)
-    client.write_single_coil(11,0)
-    time.sleep(15)
-    client.write_single_coil(6,0)
-    client.write_single_coil(7,1)
-    client.write_single_coil(8,0)
-    client.write_single_coil(9,0)
-    client.write_single_coil(10,1)
-    client.write_single_coil(11,0)
-    time.sleep(3)
+    reg_zero = client.read_holding_registers(1)
+    while reg_zero[0] == 1:
+        client.write_single_coil(0,1)
+        client.write_single_coil(1,0)
+        client.write_single_coil(2,0)
+        client.write_single_coil(3,1)
+        client.write_single_coil(4,0)
+        client.write_single_coil(5,0)
+        client.write_single_coil(6,0)
+        client.write_single_coil(7,0)
+        client.write_single_coil(8,1)
+        client.write_single_coil(9,0)
+        client.write_single_coil(10,0)
+        client.write_single_coil(11,1)
+        for x in range(15):
+            step_up()
+        client.write_single_coil(0,0)
+        client.write_single_coil(1,1)
+        client.write_single_coil(2,0)
+        client.write_single_coil(3,0)
+        client.write_single_coil(4,1)
+        client.write_single_coil(5,0)
+        for x in range(3):
+            step_up()
+        client.write_single_coil(0,0)
+        client.write_single_coil(1,0)
+        client.write_single_coil(2,1)
+        client.write_single_coil(3,0)
+        client.write_single_coil(4,0)
+        client.write_single_coil(5,1)
+        client.write_single_coil(6,1)
+        client.write_single_coil(7,0)
+        client.write_single_coil(8,0)
+        client.write_single_coil(9,1)
+        client.write_single_coil(10,0)
+        client.write_single_coil(11,0)
+        for x in range(15):
+            step_up()
+        client.write_single_coil(6,0)
+        client.write_single_coil(7,1)
+        client.write_single_coil(8,0)
+        client.write_single_coil(9,0)
+        client.write_single_coil(10,1)
+        client.write_single_coil(11,0)
+        for x in range(3):
+            step_up()
 
 def run():
 
-    step=0
-    
-        
     while traci.simulation.getMinExpectedNumber() > 0:
-        
-        #print(traci.trafficlight.getIDList())
         F1 = traci.trafficlight.getRedYellowGreenState("F1")
         F2 = traci.trafficlight.getRedYellowGreenState("F2")
         F3 = traci.trafficlight.getRedYellowGreenState("F3")
         F4 = traci.trafficlight.getRedYellowGreenState("F4")
-        print("Etat du feu rouge")
-        print("F1: " + F1)
-        print("F2: " + F2)
-        print("F3: " + F3)
-        print("F4: " + F4)
+        show_lights_state()
         ##
         traci.simulationStep()
-        step+=1
-        if client.read_holding_registers(0) != 1:
+        #print("La valeur du reg 1: " + client.read_holding_registers(1))
+        if client.read_holding_registers(1)[0] != 1:
             if (F1[0] == "G"):
                 client.write_single_coil(0,1)
                 client.write_single_coil(1,0)
